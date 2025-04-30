4b3b160b83ca43dc9defed344533adea72560aad
Jack Kelly <jack@openclimatefix.org>
2025-04-30 10:09:39 +0100

    The get_start_and_end_times script works! I've updated the documentation for the script.

--- python/solar/data-preparation/open_climate_fix_uk_pv_dataset/get_start_and_end_times_for_each_ss_id.py
+++ python/solar/data-preparation/open_climate_fix_uk_pv_dataset/get_start_and_end_times_for_each_ss_id.py
@@ -9,6 +9,12 @@ app = marimo.App(width="medium")
 @app.cell
 def _():
     import marimo as mo
+    return (mo,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md("""Read through the Parquet data to get the start and end dates for each SS_ID, and then join that into the metadata.""")
     return
 
 
@@ -24,6 +30,8 @@ def _():
 
 @app.cell
 def _(PV_DATA_PATH, pl):
+    """Load the metadata. We need a list of SS_IDs so we can tell `scan_parquet` the schema."""
+
     metadata = pl.read_csv(PV_DATA_PATH / "metadata.csv")
     ss_ids = metadata["ss_id"].to_list()
     return metadata, ss_ids
@@ -31,6 +39,8 @@ def _(PV_DATA_PATH, pl):
 
 @app.cell
 def _(PV_DATA_PATH, pl, ss_ids):
+    """Lazily open the parquet files. We need to specify the schema so that we can use `scan_parquet` with the wide tables (where each SS_ID is a column), even though each month might have a different set of columns (SS_IDs)."""
+
     schema = {"datetime_GMT": pl.Datetime("ns", "UTC")}
     schema.update({f"{ss_id}": pl.Float32 for ss_id in ss_ids})
     df = pl.scan_parquet(
@@ -43,12 +53,15 @@ def _(PV_DATA_PATH, pl, ss_ids):
 
 @app.cell
 def _(df, itertools, ss_ids):
+    """Select _batches_ of columns at once. In testing, this approach is far faster than selecting one columns at a time. We can't load _everything_ at once or Polars will quickly try to use more RAM than is available (on a 32GB machine). This takes about 2 hour and 40 minutes."""
+
     start_and_end_dates = []
     _BATCH_SIZE = 256
 
     for i, ss_id_batch in enumerate(itertools.batched(ss_ids, _BATCH_SIZE)):
         print(
-            f"\rProcessing batch of SS_IDs from number {i * _BATCH_SIZE:5,d} to {(i + 1) * _BATCH_SIZE:5,d}. Total SS_IDs = {len(ss_ids):,d}",
+            f"\rProcessing batch of SS_IDs from number {i * _BATCH_SIZE:5,d} to {(i + 1) * _BATCH_SIZE:5,d}."
+            " Total SS_IDs = {len(ss_ids):,d}",
             end="",
         )
         cols = ["datetime_GMT"] + list(map(str, ss_id_batch))
@@ -67,9 +80,22 @@ def _(df, itertools, ss_ids):
 
 @app.cell
 def _(PV_DATA_PATH, metadata, pl, start_and_end_dates):
+    """Save as 'metadata_with_dates.csv'. So you can check the CSV before overwriting 'metadata.csv'."""
+
     pl.from_dicts(start_and_end_dates).join(metadata, on="ss_id").write_csv(
         PV_DATA_PATH / "metadata_with_dates.csv", datetime_format="%Y-%m-%dT%H:%M:%SZ"
     )
+    return
+
+
+@app.cell
+def _(PV_DATA_PATH, pl):
+    pl.read_csv(PV_DATA_PATH / "metadata_with_dates.csv")
+    return
+
+
+@app.cell
+def _():
     return
 
 
